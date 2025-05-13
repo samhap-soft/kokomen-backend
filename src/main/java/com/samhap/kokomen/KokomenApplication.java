a623b6890c2eff654eec936bcf4eca6e1d7ab017
devhoya97 <diajbh123@gmail.com>
2025-05-13 20:54:58 +0900

    chore: ci 정상동작 테스트

--- src/main/java/com/samhap/kokomen/KokomenApplication.java
+++ src/main/java/com/samhap/kokomen/KokomenApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KokomenApplication {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(KokomenApplication.class, args);
 	}
