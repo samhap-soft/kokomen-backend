f342450370fb0d29a88a152905f5b212b2e2e24b
nhlee98 <e5slnh9876@gmail.com>
2025-05-21 17:31:32 +0900

    chore: restDocs 관련 설정

--- src/test/java/com/samhap/kokomen/category/controller/CategoryControllerTest.java
+++ src/test/java/com/samhap/kokomen/category/controller/CategoryControllerTest.java
@@ -1,25 +1,60 @@
 package com.samhap.kokomen.category.controller;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@AutoConfigureMockMvc
+@ExtendWith(RestDocumentationExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 class CategoryControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
+    @BeforeEach
+    void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .alwaysDo(print())
+                .apply(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(
+                                modifyUris()
+                                        .scheme("https")
+                                        .host("api.dev.kokomen.kr")
+                                        .removePort(),
+                                prettyPrint(),
+                                modifyHeaders().remove(HttpHeaders.CONTENT_LENGTH)
+                        )
+                        .withResponseDefaults(
+                                prettyPrint(),
+                                modifyHeaders().remove(HttpHeaders.CONTENT_LENGTH)
+                        )
+                )
+                .build();
+    }
+
     @Test
-    void findCategories() throws Exception {
+    void 카테고리_목록을_조회한다() throws Exception {
         // given
         String expectedJson = """
                 {
@@ -36,6 +71,10 @@ class CategoryControllerTest {
         // when & then
         mockMvc.perform(get("/categories"))
                 .andExpect(status().isOk())
-                .andExpect(content().json(expectedJson));
+                .andExpect(content().json(expectedJson))
+                .andDo(document("category",
+                        responseFields(
+                                fieldWithPath("categories").type(ARRAY).description("카테고리 목록")
+                        )));
     }
 }
