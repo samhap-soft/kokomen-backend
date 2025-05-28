0049856e569eb20c0f46d9a1d4a19c396a46bf15
nhlee98 <e5slnh9876@gmail.com>
2025-05-28 16:35:55 +0900

    refactor: 버저닝을 고려한 API URL 변경

--- src/test/java/com/samhap/kokomen/category/controller/CategoryControllerTest.java
+++ src/test/java/com/samhap/kokomen/category/controller/CategoryControllerTest.java
@@ -1,52 +1,17 @@
 package com.samhap.kokomen.category.controller;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.samhap.kokomen.global.BaseControllerTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.HttpHeaders;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(RestDocumentationExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
-class CategoryControllerTest {
-
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
-        var uriPreprocessor = modifyUris()
-                .scheme("https")
-                .host("api.dev.kokomen.kr")
-                .removePort();
-
-        var headerPreprocessor = modifyHeaders().remove(HttpHeaders.CONTENT_LENGTH);
-
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .alwaysDo(print())
-                .apply(documentationConfiguration(restDocumentation).operationPreprocessors()
-                        .withRequestDefaults(uriPreprocessor, prettyPrint(), headerPreprocessor)
-                        .withResponseDefaults(prettyPrint(), headerPreprocessor)
-                ).build();
-    }
+class CategoryControllerTest extends BaseControllerTest {
 
     @Test
     void 카테고리_목록을_조회한다() throws Exception {
@@ -64,7 +29,7 @@ class CategoryControllerTest {
                 """;
 
         // when & then
-        mockMvc.perform(get("/categories"))
+        mockMvc.perform(get("/api/v1/categories"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedJson))
                 .andDo(document("category",
