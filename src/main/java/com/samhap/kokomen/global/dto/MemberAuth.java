3a2c3b813751cb965b493651d239df31a5675fc5
nhlee98 <e5slnh9876@gmail.com>
2025-05-28 16:28:57 +0900

    feat: 멤버 관련 클래스 추가

---
+++ src/main/java/com/samhap/kokomen/global/dto/MemberAuth.java
@@ -0,0 +1,6 @@
+package com.samhap.kokomen.global.dto;
+
+public record MemberAuth(
+        Long memberId
+) {
+}
---
+++ src/main/java/com/samhap/kokomen/member/domain/Member.java
@@ -0,0 +1,27 @@
+package com.samhap.kokomen.member.domain;
+
+import com.samhap.kokomen.global.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member extends BaseEntity {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
