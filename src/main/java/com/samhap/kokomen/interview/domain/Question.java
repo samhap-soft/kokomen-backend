a7a66cd2330e2153ff956c8a3af227276a9694e9
nhlee98 <e5slnh9876@gmail.com>
2025-05-28 16:31:41 +0900

    feat: 인터뷰 관련 도메인 클래스 추가

---
+++ src/main/java/com/samhap/kokomen/interview/domain/Question.java
@@ -0,0 +1,42 @@
+package com.samhap.kokomen.interview.domain;
+
+import com.samhap.kokomen.global.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Question extends BaseEntity {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interview_id", nullable = false)
+    private Interview interview;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "root_question_id", nullable = false)
+    private RootQuestion rootQuestion;
+
+    @Column(name = "content", nullable = false, length = 1_000)
+    private String content;
+
+    public Question(Interview interview, RootQuestion rootQuestion, String content) {
+        this.interview = interview;
+        this.rootQuestion = rootQuestion;
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/samhap/kokomen/interview/domain/RootQuestion.java
@@ -0,0 +1,29 @@
+package com.samhap.kokomen.interview.domain;
+
+import com.samhap.kokomen.global.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class RootQuestion extends BaseEntity {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "content", nullable = false, length = 1_000)
+    private String content;
+
+    public RootQuestion(String content) {
+        this.content = content;
+    }
+}
