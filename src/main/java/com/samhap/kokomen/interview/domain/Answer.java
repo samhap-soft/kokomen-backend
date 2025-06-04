90dd0593c0a8363011a0e7a6cb96b84a273a1554
nhlee98 <e5slnh9876@gmail.com>
2025-06-04 16:44:15 +0900

    refactor: 엔티티에 @AllArgConstructor 추가

--- src/main/java/com/samhap/kokomen/interview/domain/Answer.java
+++ src/main/java/com/samhap/kokomen/interview/domain/Answer.java
@@ -12,10 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Answer extends BaseEntity {
