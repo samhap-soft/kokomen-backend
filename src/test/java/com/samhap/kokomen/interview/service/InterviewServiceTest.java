9f8fe3bc2759b80eae0395c467bf00d980fd9495
nhlee98 <e5slnh9876@gmail.com>
2025-06-05 18:08:24 +0900

    test: 인터뷰 진행 서비스 테스트 코드 작성

--- src/test/java/com/samhap/kokomen/interview/service/InterviewServiceTest.java
+++ src/test/java/com/samhap/kokomen/interview/service/InterviewServiceTest.java
@@ -1,18 +1,32 @@
 package com.samhap.kokomen.interview.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import com.samhap.kokomen.global.BaseTest;
-import com.samhap.kokomen.interview.domain.Answer;
+import com.samhap.kokomen.global.dto.MemberAuth;
+import com.samhap.kokomen.global.fixture.interview.AnswerFixtureBuilder;
+import com.samhap.kokomen.global.fixture.interview.GptResponseFixtureBuilder;
+import com.samhap.kokomen.global.fixture.interview.InterviewFixtureBuilder;
+import com.samhap.kokomen.global.fixture.interview.QuestionFixtureBuilder;
+import com.samhap.kokomen.global.fixture.interview.RootQuestionFixtureBuilder;
+import com.samhap.kokomen.global.fixture.member.MemberFixtureBuilder;
 import com.samhap.kokomen.interview.domain.AnswerRank;
 import com.samhap.kokomen.interview.domain.Interview;
 import com.samhap.kokomen.interview.domain.Question;
 import com.samhap.kokomen.interview.domain.RootQuestion;
+import com.samhap.kokomen.interview.external.dto.response.GptResponse;
 import com.samhap.kokomen.interview.repository.AnswerRepository;
 import com.samhap.kokomen.interview.repository.InterviewRepository;
 import com.samhap.kokomen.interview.repository.QuestionRepository;
 import com.samhap.kokomen.interview.repository.RootQuestionRepository;
 import com.samhap.kokomen.interview.service.dto.AnswerRequest;
+import com.samhap.kokomen.interview.service.dto.NextQuestionResponse;
 import com.samhap.kokomen.member.domain.Member;
 import com.samhap.kokomen.member.repository.MemberRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,9 +35,9 @@ class InterviewServiceTest extends BaseTest {
     @Autowired
     private InterviewService interviewService;
     @Autowired
-    private AnswerRepository answerRepository;
-    @Autowired
     private InterviewRepository interviewRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
     @Autowired
     private QuestionRepository questionRepository;
     @Autowired
@@ -32,17 +46,61 @@ class InterviewServiceTest extends BaseTest {
     private RootQuestionRepository rootQuestionRepository;
 
     @Test
-    void proceedInterview() {
+    void 인터뷰를_진행할_때_마지막_답변이_아니면_다음_꼬리_질문과_현재_답변에_대한_피드백을_응답한다() {
         // given
-        Member member = memberRepository.save(new Member("NAK"));
-        Interview interview = interviewRepository.save(new Interview(member));
-        RootQuestion rootQuestion = rootQuestionRepository.save(new RootQuestion("자바의 특징은 무엇인가요?"));
-        Question question1 = questionRepository.save(new Question(interview, rootQuestion, rootQuestion.getContent()));
-        Answer answer1 = answerRepository.save(new Answer(question1, "자바는 객체지향 프로그래밍 언어입니다.", AnswerRank.C, "부족합니다."));
+        Member member = memberRepository.save(MemberFixtureBuilder.builder().build());
+        Interview interview = interviewRepository.save(InterviewFixtureBuilder.builder().member(member).build());
+        RootQuestion rootQuestion = rootQuestionRepository.save(RootQuestionFixtureBuilder.builder().build());
+        Question question = questionRepository.save(QuestionFixtureBuilder.builder().rootQuestion(rootQuestion).build());
+        String nextQuestion = "스레드 안전하다는 것은 무엇인가요?";
+        GptResponse gptResponse = GptResponseFixtureBuilder.builder()
+                .nextQuestion(nextQuestion)
+                .buildProceed();
+        when(gptClient.requestToGpt(any())).thenReturn(gptResponse);
 
-        Question question2 = questionRepository.save(new Question(interview, rootQuestion, "객체지향의 특징을 설명해주세요."));
-        AnswerRequest answerRequest = new AnswerRequest("절차지향 프로그래밍과 반대되는 개념입니다.");
+        NextQuestionResponse expected = new NextQuestionResponse(question.getId() + 1, nextQuestion, false);
 
-//        interviewService.feedbackAnswerAndCreateNextQuestion(interview.getId(), question2.getId(), answerRequest, null);
+        // when
+        Optional<NextQuestionResponse> actual = interviewService.proceedInterview(
+                interview.getId(), question.getId(), new AnswerRequest("프로세스는 무겁고, 스레드는 가벼워요."), new MemberAuth(member.getId()));
+
+        // then
+        assertAll(
+                () -> assertThat(actual).contains(expected),
+                () -> assertThat(questionRepository.existsById(question.getId() + 1)).isTrue()
+        );
+    }
+
+    @Test
+    void 인터뷰를_진행할_때_마지막_답변이면_현재_답변에_대한_피드백과__응답한다() {
+        // given
+        AnswerRank answerRank = AnswerRank.B;
+        Member member = memberRepository.save(MemberFixtureBuilder.builder().build());
+        Interview interview = interviewRepository.save(InterviewFixtureBuilder.builder().member(member).build());
+        RootQuestion rootQuestion = rootQuestionRepository.save(RootQuestionFixtureBuilder.builder().build());
+        Question question1 = questionRepository.save(QuestionFixtureBuilder.builder().rootQuestion(rootQuestion).build());
+        Question question2 = questionRepository.save(QuestionFixtureBuilder.builder().rootQuestion(rootQuestion).build());
+        Question question3 = questionRepository.save(QuestionFixtureBuilder.builder().rootQuestion(rootQuestion).build());
+        answerRepository.save(AnswerFixtureBuilder.builder().question(question1).answerRank(answerRank).build());
+        answerRepository.save(AnswerFixtureBuilder.builder().question(question2).answerRank(answerRank).build());
+        String totalFeedback = "스레드 안전하다는 것은 무엇인가요?";
+        GptResponse gptResponse = GptResponseFixtureBuilder.builder()
+                .totalFeedback(totalFeedback)
+                .answerRank(answerRank)
+                .buildEnd();
+        when(gptClient.requestToGpt(any())).thenReturn(gptResponse);
+
+        // when
+        Optional<NextQuestionResponse> actual = interviewService.proceedInterview(
+                interview.getId(), question3.getId(), new AnswerRequest("프로세스는 무겁고, 스레드는 가벼워요."), new MemberAuth(member.getId()));
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isEmpty(),
+                () -> assertThat(questionRepository.existsById(question3.getId() + 1)).isFalse(),
+                () -> assertThat(interviewRepository.findById(interview.getId()).get().getTotalFeedback()).isEqualTo(totalFeedback),
+                () -> assertThat(interviewRepository.findById(interview.getId()).get().getTotalScore()).isEqualTo(answerRank.getScore() * 3),
+                () -> assertThat(memberRepository.findById(member.getId()).get().getScore()).isEqualTo(member.getScore() + answerRank.getScore() * 3)
+        );
     }
 }
