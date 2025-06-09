f8ed88a41f04b1abac0f1641b1264b0ea3953fb6
devhoya97 <diajbh123@gmail.com>
2025-06-09 22:19:24 +0900

    refactor: 루트 질문을 가져오는 쿼리에서 NOT IN을 사용하지 않는 방향으로 수정

--- src/main/java/com/samhap/kokomen/interview/repository/RootQuestionRepository.java
+++ src/main/java/com/samhap/kokomen/interview/repository/RootQuestionRepository.java
@@ -11,21 +11,20 @@ public interface RootQuestionRepository extends JpaRepository<RootQuestion, Long
     @Query(value = """
             SELECT rq.*
             FROM root_question rq
+            LEFT JOIN (
+                SELECT i.root_question_id
+                FROM interview i
+                JOIN root_question sub_rq ON sub_rq.id = i.root_question_id
+                WHERE i.member_id = :memberId
+                  AND sub_rq.category = :category
+                ORDER BY i.id DESC
+                LIMIT :recentLimit
+            ) recent
+            ON rq.id = recent.root_question_id
             WHERE rq.category = :category
-              AND rq.id NOT IN (
-                SELECT recent.root_question_id
-                FROM (
-                  SELECT i.root_question_id
-                  FROM interview i
-                  JOIN root_question sub_rq ON sub_rq.id = i.root_question_id
-                  WHERE i.member_id = :memberId
-                    AND sub_rq.category = :category
-                  ORDER BY i.id DESC
-                  LIMIT :recentLimit
-                ) AS recent
-              )
+              AND recent.root_question_id IS NULL
             ORDER BY RAND()
-            LIMIT 1;
+            LIMIT 1
             """, nativeQuery = true)
     Optional<RootQuestion> findRandomByCategoryExcludingRecent(
             @Param("memberId") Long memberId,
--- src/test/java/com/samhap/kokomen/interview/repository/RootQuestionRepositoryTest.java
+++ src/test/java/com/samhap/kokomen/interview/repository/RootQuestionRepositoryTest.java
@@ -17,18 +17,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 class RootQuestionRepositoryTest extends BaseTest {
 
     @Autowired
-    private AnswerRepository answerRepository;
-    @Autowired
     private InterviewRepository interviewRepository;
-    @Autowired
-    private QuestionRepository questionRepository;
     @Autowired
     private MemberRepository memberRepository;
     @Autowired
     private RootQuestionRepository rootQuestionRepository;
 
     @Test
-    void 사용자가_최근에_받지_않았던_루트_질문_중에서_랜덤으로_고른다() {
+    void 사용자가_받지_않았던_루트_질문_중에서_랜덤으로_고른다() {
         // given
         RootQuestion operatingSystemRootQuestion1 = rootQuestionRepository.save(
                 RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
@@ -48,5 +44,52 @@ class RootQuestionRepositoryTest extends BaseTest {
                 .get()
                 .extracting(RootQuestion::getId)
                 .isEqualTo(operatingSystemRootQuestion2.getId());
+    }
+
+    @Test
+    void 사용자가_받았던_루트_질문이더라도_최근에_받지_않았다면_중복해서_고를_수_있다() {
+        // given
+        RootQuestion operatingSystemRootQuestion1 = rootQuestionRepository.save(
+                RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
+        RootQuestion operatingSystemRootQuestion2 = rootQuestionRepository.save(
+                RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
+        RootQuestion operatingSystemRootQuestion3 = rootQuestionRepository.save(
+                RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
+        Member member = memberRepository.save(MemberFixtureBuilder.builder().build());
+        interviewRepository.save(InterviewFixtureBuilder.builder().rootQuestion(operatingSystemRootQuestion1).member(member).build());
+        interviewRepository.save(InterviewFixtureBuilder.builder().rootQuestion(operatingSystemRootQuestion2).member(member).build());
+        interviewRepository.save(InterviewFixtureBuilder.builder().rootQuestion(operatingSystemRootQuestion3).member(member).build());
+
+        // when
+        Optional<RootQuestion> result = rootQuestionRepository.findRandomByCategoryExcludingRecent(member.getId(), Category.OPERATING_SYSTEM.name(), 2);
+
+        // then
+        assertThat(result)
+                .isPresent()
+                .get()
+                .extracting(RootQuestion::getId)
+                .isEqualTo(operatingSystemRootQuestion1.getId());
+    }
+
+    @Test
+    void 사용자가_최근에_받았던_질문을_제외했을때_남은_루트_질문이_없다면_empty를_반환한다() {
+        // given
+        RootQuestion operatingSystemRootQuestion1 = rootQuestionRepository.save(
+                RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
+        RootQuestion operatingSystemRootQuestion2 = rootQuestionRepository.save(
+                RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
+        RootQuestion operatingSystemRootQuestion3 = rootQuestionRepository.save(
+                RootQuestionFixtureBuilder.builder().category(Category.OPERATING_SYSTEM).build());
+        rootQuestionRepository.save(RootQuestionFixtureBuilder.builder().category(Category.DATA_STRUCTURE).build());
+        Member member = memberRepository.save(MemberFixtureBuilder.builder().build());
+        interviewRepository.save(InterviewFixtureBuilder.builder().rootQuestion(operatingSystemRootQuestion1).member(member).build());
+        interviewRepository.save(InterviewFixtureBuilder.builder().rootQuestion(operatingSystemRootQuestion2).member(member).build());
+        interviewRepository.save(InterviewFixtureBuilder.builder().rootQuestion(operatingSystemRootQuestion3).member(member).build());
+
+        // when
+        Optional<RootQuestion> result = rootQuestionRepository.findRandomByCategoryExcludingRecent(member.getId(), Category.OPERATING_SYSTEM.name(), 3);
+
+        // then
+        assertThat(result).isEmpty();
     }
 }
