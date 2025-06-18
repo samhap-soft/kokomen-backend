c6f3fb0158694b6d2a6b7148a18ec7b0799b9d78
nhlee98 <e5slnh9876@gmail.com>
2025-06-18 20:52:34 +0900

    feat: 카카오 인가코드를 받아 로그인을 수행하는 기능 구현

--- src/main/java/com/samhap/kokomen/auth/controller/AuthController.java
+++ src/main/java/com/samhap/kokomen/auth/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,31 +21,27 @@ public class AuthController {
     private final AuthService authService;
     private final String clientId;
     private final String redirectUri;
-    private final String frontendUri;
 
     public AuthController(
             AuthService authService,
             @Value("${oauth.kakao.client-id}") String clientId,
-            @Value("${oauth.kakao.redirect-uri}") String redirectUri,
-            @Value("${oauth.frontend-uri}") String frontendUri
+            @Value("${oauth.kakao.redirect-uri}") String redirectUri
     ) {
         this.authService = authService;
         this.clientId = clientId;
         this.redirectUri = redirectUri;
-        this.frontendUri = frontendUri;
     }
 
     @GetMapping("/kakao-login")
-    public ResponseEntity<Void> kakaoLogin() {
+    public ResponseEntity<Void> redirectKakaoLoginPage() {
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(
                         URI.create("https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s".formatted(clientId, redirectUri)))
                 .build();
     }
 
-    // TODO: 이 API에서 에러가 발생해도 프론트엔드로 리디렉션이 되도록 수정 필요
-    @GetMapping("/kakao-login/redirect")
-    public ResponseEntity<Void> kakaoLoginRedirect(
+    @PostMapping("/kakao-login")
+    public ResponseEntity<MemberResponse> kakaoLogin(
             @RequestParam String code,
             HttpServletRequest request
     ) {
@@ -53,8 +50,6 @@ public class AuthController {
         HttpSession session = request.getSession(true);
         session.setAttribute("MEMBER_ID", memberResponse.id());
 
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .location(URI.create(frontendUri))
-                .build();
+        return ResponseEntity.ok(memberResponse);
     }
 }
--- src/test/java/com/samhap/kokomen/auth/controller/AuthControllerTest.java
+++ src/test/java/com/samhap/kokomen/auth/controller/AuthControllerTest.java
@@ -1,13 +1,23 @@
 package com.samhap.kokomen.auth.controller;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.samhap.kokomen.auth.external.dto.KakaoAccount;
+import com.samhap.kokomen.auth.external.dto.KakaoUserInfoResponse;
+import com.samhap.kokomen.auth.external.dto.Profile;
 import com.samhap.kokomen.global.BaseControllerTest;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -24,9 +34,41 @@ class AuthControllerTest extends BaseControllerTest {
                 .andExpect(status().isFound())
                 .andExpect(header().string("Location",
                         "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=%s&redirect_uri=%s".formatted(clientId, redirectUri)))
-                .andDo(document("auth-kakaoLogin",
+                .andDo(document("auth-redirectKakaoLoginPage",
                         responseHeaders(
                                 headerWithName("Location").description("카카오 로그인 페이지를 받을 리다이렉션 URI")
+                        )
+                ));
+    }
+
+    @Test
+    void 카카오_로그인_성공() throws Exception {
+        String code = "test-code";
+        String nickname = "오상훈";
+        String responseJson = """
+                {
+                  "id": 1,
+                  "nickname": "%s"
+                }
+                """.formatted(nickname);
+        when(kakaoOAuthClient.requestKakaoUserInfo(code)).thenReturn(new KakaoUserInfoResponse(1L, new KakaoAccount(new Profile(nickname))));
+
+        mockMvc.perform(post("/api/v1/auth/kakao-login")
+                        .param("code", code))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson))
+                .andDo(result -> {
+                    HttpSession session = result.getRequest().getSession(false);
+                    result.getResponse().addHeader("Set-Cookie", "JSESSIONID=" + session.getId());
+                })
+                .andExpect(header().string("Set-Cookie", containsString("JSESSIONID=")))
+                .andDo(document("auth-kakaoLogin",
+                        responseHeaders(
+                                headerWithName("Set-Cookie").description("로그인 성공 후 세션 쿠키")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("멤버 id"),
+                                fieldWithPath("nickname").description("멤버 닉네임")
                         )
                 ));
     }
