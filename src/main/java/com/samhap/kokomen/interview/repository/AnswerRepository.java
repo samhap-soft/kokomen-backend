263d3b2fd4a6a72ab203650161e59a0bb57ce0e9
nhlee98 <e5slnh9876@gmail.com>
2025-06-27 03:39:02 +0900

    feat: ID 순으로 정렬하는 JPA 쿼리 메소드 추가

--- src/main/java/com/samhap/kokomen/interview/repository/AnswerRepository.java
+++ src/main/java/com/samhap/kokomen/interview/repository/AnswerRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
     List<Answer> findByQuestionIn(List<Question> questions);
+
+    List<Answer> findByQuestionInOrderById(List<Question> questions);
 }
