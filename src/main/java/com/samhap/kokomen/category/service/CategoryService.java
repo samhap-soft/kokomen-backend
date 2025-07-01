37aaeb0ee0dfaca2f72256a16ff0ffede1e7c0f8
와일드카고 <146502065+kargowild@users.noreply.github.com>
2025-07-01 13:36:58 +0900

    feat: 면접 카테고리 목록 조회 API 수정 (#90)

--- src/main/java/com/samhap/kokomen/category/service/CategoryService.java
+++ src/main/java/com/samhap/kokomen/category/service/CategoryService.java
@@ -2,12 +2,16 @@ package com.samhap.kokomen.category.service;
 
 import com.samhap.kokomen.category.domain.Category;
 import com.samhap.kokomen.category.service.dto.CategoryResponse;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
 
-    public CategoryResponse findCategories() {
-        return new CategoryResponse(Category.getCategories());
+    public List<CategoryResponse> findCategories() {
+        return Category.getCategories()
+                .stream()
+                .map(CategoryResponse::new)
+                .toList();
     }
 }
