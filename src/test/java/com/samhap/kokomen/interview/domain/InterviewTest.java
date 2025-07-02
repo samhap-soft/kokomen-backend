fa2721a88e9a76d26bba37ec33b3e117c5345c31
devhoya97 <diajbh123@gmail.com>
2025-07-02 16:22:00 +0900

    test: 테스트 경계값 수정

--- src/test/java/com/samhap/kokomen/interview/domain/InterviewTest.java
+++ src/test/java/com/samhap/kokomen/interview/domain/InterviewTest.java
@@ -11,16 +11,16 @@ import org.junit.jupiter.params.provider.ValueSource;
 class InterviewTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {2, 11})
-    void 최대_질문_개수가_3미만_10초과라면_예외가_발생한다(int maxQuestionCount) {
+    @ValueSource(ints = {2, 21})
+    void 최대_질문_개수가_3미만_20초과라면_예외가_발생한다(int maxQuestionCount) {
         assertThatThrownBy(() -> InterviewFixtureBuilder.builder().maxQuestionCount(maxQuestionCount).build())
                 .isInstanceOf(BadRequestException.class)
                 .hasMessageContaining("최대 질문 개수는");
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 10})
-    void 최대_질문_개수가_2이상_10이하라면_인터뷰_생성에_성공한다(int maxQuestionCount) {
+    @ValueSource(ints = {3, 20})
+    void 최대_질문_개수가_2이상_20이하라면_인터뷰_생성에_성공한다(int maxQuestionCount) {
         assertThatCode(() -> InterviewFixtureBuilder.builder().maxQuestionCount(maxQuestionCount).build())
                 .doesNotThrowAnyException();
     }
