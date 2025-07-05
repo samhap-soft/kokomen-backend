5cadf5f68c1e509fae6cae7ba615f3ffe704523f
와일드카고 <146502065+kargowild@users.noreply.github.com>
2025-07-05 16:48:25 +0900

    [Feat] - 조회수, 좋아요 기능을 위한 엔티티 수정 (#119)
    * feat: 좋아요 수, 조회 수 관련하여 수정
    
    * fix: UNIQUE KEY는 H2에서 예외가 발생하므로 CONSTRAINT로 변경

---
+++ src/test/java/com/samhap/kokomen/global/fixture/interview/AnswerLikeFixtureBuilder.java
@@ -0,0 +1,50 @@
+package com.samhap.kokomen.global.fixture.interview;
+
+import com.samhap.kokomen.global.fixture.member.MemberFixtureBuilder;
+import com.samhap.kokomen.interview.domain.Answer;
+import com.samhap.kokomen.interview.domain.AnswerLike;
+import com.samhap.kokomen.member.domain.Member;
+
+public class AnswerLikeFixtureBuilder {
+
+    private Long id;
+    private Member member;
+    private Answer answer;
+
+    public static AnswerLikeFixtureBuilder builder() {
+        return new AnswerLikeFixtureBuilder();
+    }
+
+    public AnswerLikeFixtureBuilder id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public AnswerLikeFixtureBuilder member(Member member) {
+        this.member = member;
+        return this;
+    }
+
+    public AnswerLikeFixtureBuilder answer(Answer answer) {
+        this.answer = answer;
+        return this;
+    }
+
+    public AnswerLike build() {
+        return new AnswerLike(
+                id,
+                member != null ? member : defaultMember(),
+                answer != null ? answer : defaultAnswer()
+        );
+    }
+
+    private static Member defaultMember() {
+        return MemberFixtureBuilder.builder()
+                .build();
+    }
+
+    private static Answer defaultAnswer() {
+        return AnswerFixtureBuilder.builder()
+                .build();
+    }
+}
