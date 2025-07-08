0dbc2ad178780394bd2e07b1326fd5fa35843ab2
와일드카고 <146502065+kargowild@users.noreply.github.com>
2025-07-08 13:57:37 +0900

    [Feat] - Interview에 대한 좋아요 API (#126)
    * feat: 좋아요 추가,취소 요청 기능 구현
    
    * refactor: 테이블 레벨에서 좋아요 갯수가 0으로 내려가지 않도록 관리
    
    * test: 좋아요 감소 테스트 메서드 변경
    
    * refactor: DataIntegrityViolationException에서 예상치 못한 예외를 잡아 생길 수 있는 문제 방지

---
+++ src/main/java/com/samhap/kokomen/interview/repository/InterviewLikeRepository.java
@@ -0,0 +1,13 @@
+package com.samhap.kokomen.interview.repository;
+
+import com.samhap.kokomen.interview.domain.Interview;
+import com.samhap.kokomen.interview.domain.InterviewLike;
+import com.samhap.kokomen.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewLikeRepository extends JpaRepository<InterviewLike, Long> {
+
+    int deleteByMemberAndInterview(Member member, Interview interview);
+
+    boolean existsByMemberIdAndInterviewId(Long memberId, Long interviewId);
+}
