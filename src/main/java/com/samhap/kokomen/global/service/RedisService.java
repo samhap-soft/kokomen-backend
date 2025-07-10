db4f8a27dfb35d4ccb09dcf226d18b0b3139a969
nhlee98 <e5slnh9876@gmail.com>
2025-07-10 18:00:17 +0900

    feat: 레디스 scan 및 multiGet 기능 구현

--- src/main/java/com/samhap/kokomen/global/service/RedisService.java
+++ src/main/java/com/samhap/kokomen/global/service/RedisService.java
@@ -1,9 +1,15 @@
 package com.samhap.kokomen.global.service;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -39,6 +45,26 @@ public class RedisService {
         }
 
         return expireSuccess;
+    }
+
+    public Cursor<String> scanKeys(String pattern, int scanCount) {
+        ScanOptions scanOptions = ScanOptions.scanOptions()
+                .match(pattern)
+                .count(scanCount)
+                .build();
+
+        return redisTemplate.scan(scanOptions);
+    }
+
+    public Map<String, Object> multiGet(List<String> keys) {
+        List<Object> values = redisTemplate.opsForValue().multiGet(keys);
+        if (values == null) {
+            throw new IllegalStateException("Redis 멀티 GET 실패. keys: " + keys);
+        }
+
+        return IntStream.range(0, keys.size())
+                .boxed()
+                .collect(Collectors.toMap(keys::get, values::get));
     }
 
     public void releaseLock(String lockKey) {
