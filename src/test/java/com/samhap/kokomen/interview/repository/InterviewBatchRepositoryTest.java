91d8909801f84453f6b1b611eea3d0ba9c0fdb59
nhlee98 <e5slnh9876@gmail.com>
2025-07-10 17:57:53 +0900

    feat: 인터뷰 조회수 배치 업데이트 SQL을 실행하는 기능 구현

---
+++ src/test/java/com/samhap/kokomen/interview/repository/InterviewBatchRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.samhap.kokomen.interview.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.samhap.kokomen.global.BaseTest;
+import com.samhap.kokomen.global.fixture.interview.InterviewFixtureBuilder;
+import com.samhap.kokomen.global.fixture.interview.RootQuestionFixtureBuilder;
+import com.samhap.kokomen.global.fixture.member.MemberFixtureBuilder;
+import com.samhap.kokomen.interview.domain.InterviewState;
+import com.samhap.kokomen.interview.domain.RootQuestion;
+import com.samhap.kokomen.member.domain.Member;
+import com.samhap.kokomen.member.repository.MemberRepository;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class InterviewBatchRepositoryTest extends BaseTest {
+
+    @Autowired
+    private InterviewBatchRepository interviewBatchRepository;
+    @Autowired
+    private InterviewRepository interviewRepository;
+    @Autowired
+    private RootQuestionRepository rootQuestionRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void batchUpdateInterviewViewCount() {
+        // given
+        int interviewCount = 15;
+        int batch_size = 3;
+        Member member = memberRepository.save(MemberFixtureBuilder.builder().build());
+        RootQuestion rootQuestion = rootQuestionRepository.save(RootQuestionFixtureBuilder.builder().build());
+        Map<Long, Long> interviewViewCounts = new HashMap<>();
+
+        for (int i = 0; i < interviewCount; i++) {
+            long interviewId = (long) i + 1;
+            long viewCount = interviewId;
+            interviewRepository.save(
+                    InterviewFixtureBuilder.builder().member(member).rootQuestion(rootQuestion).interviewState(InterviewState.FINISHED).build());
+            interviewViewCounts.put(interviewId, viewCount);
+
+        }
+
+        // when
+        interviewBatchRepository.batchUpdateInterviewViewCount(interviewViewCounts, batch_size);
+
+        // then
+        for (long interviewId = 1; interviewId <= interviewCount; interviewId++) {
+            Long viewCount = interviewRepository.findById(interviewId).get().getViewCount();
+            assertThat(viewCount).isEqualTo(interviewViewCounts.get(interviewId));
+        }
+    }
+}
