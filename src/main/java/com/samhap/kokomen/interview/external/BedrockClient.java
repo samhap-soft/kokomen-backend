c854c81eca2867e0321c0382799b876985aee18a
devhoya97 <diajbh123@gmail.com>
2025-07-19 14:07:19 +0900

    feat: Bedrock 요청 시 JSON으로 응답받기 위한 ToolConfiguration 추가

--- src/main/java/com/samhap/kokomen/interview/external/BedrockClient.java
+++ src/main/java/com/samhap/kokomen/interview/external/BedrockClient.java
@@ -10,10 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 import org.springframework.web.client.RestClientResponseException;
+import software.amazon.awssdk.core.document.Document;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
 import software.amazon.awssdk.services.bedrockruntime.model.ConverseRequest;
 import software.amazon.awssdk.services.bedrockruntime.model.ConverseResponse;
 import software.amazon.awssdk.services.bedrockruntime.model.Message;
+import software.amazon.awssdk.services.bedrockruntime.model.Tool;
+import software.amazon.awssdk.services.bedrockruntime.model.ToolConfiguration;
+import software.amazon.awssdk.services.bedrockruntime.model.ToolSpecification;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -56,11 +60,75 @@ public class BedrockClient {
                 .messages(messages);
 
         if (questionAndAnswers.isProceedRequest()) {
-            builder.system(InterviewMessagesFactory.createBedrockProceedSystemMessage());
+            Document document = Document.fromString("""
+                    {
+                        "type": "object",
+                        "properties": {
+                            "rank": {
+                                "type": "string",
+                                "description": "사용자 답변에 대한 평가 점수",
+                                "enum": ["A", "B", "C", "D", "F"]
+                            },
+                            "feedback": {
+                                "type": "string",
+                                "description": "사용자 답변에 대한 피드백"
+                            },
+                            "next_question": {
+                                "type": "string",
+                                "description": "현재 문맥과 사용자 답변을 바탕으로 생성된 다음 꼬리 질문"
+                            }
+                        },
+                        "required": ["rank", "feedback", "next_question"]
+                    }
+                    """);
+            Tool tool = Tool.builder()
+                    .toolSpec(ToolSpecification.builder()
+                            .name("답변에 대한 평가 및 꼬리 질문 생성")
+                            .description("답변에 대한 평가 및 꼬리 질문 생성")
+                            .inputSchema(schemaBuilder -> schemaBuilder.json(document)).build())
+                    .build();
+            ToolConfiguration toolConfiguration = ToolConfiguration.builder()
+                    .tools(List.of(tool))
+                    .build();
+
+            builder.toolConfig(toolConfiguration)
+                    .system(InterviewMessagesFactory.createBedrockProceedSystemMessage());
             return builder.build();
         }
 
-        builder.system(InterviewMessagesFactory.createBedrockEndSystemMessage());
+        Document document = Document.fromString("""
+                {
+                    "type": "object",
+                    "properties": {
+                        "rank": {
+                            "type": "string",
+                            "description": "사용자 답변에 대한 평가 점수",
+                            "enum": ["A", "B", "C", "D", "F"]
+                        },
+                        "feedback": {
+                            "type": "string",
+                            "description": "사용자 답변에 대한 피드백"
+                        },
+                        "total_feedback": {
+                            "type": "string",
+                            "description": "전체 면접에 대한 피드백"
+                        }
+                    },
+                    "required": ["rank", "feedback", "total_feedback"]
+                }
+                """);
+        Tool tool = Tool.builder()
+                .toolSpec(ToolSpecification.builder()
+                        .name("면접 마지막 질문에 대한 평가 및 전체 면접에 대한 피드백")
+                        .description("면접 마지막 질문에 대한 평가 및 전체 면접에 대한 피드백")
+                        .inputSchema(schemaBuilder -> schemaBuilder.json(document)).build())
+                .build();
+        ToolConfiguration toolConfiguration = ToolConfiguration.builder()
+                .tools(List.of(tool))
+                .build();
+
+        builder.toolConfig(toolConfiguration)
+                .system(InterviewMessagesFactory.createBedrockEndSystemMessage());
         return builder.build();
     }
 
