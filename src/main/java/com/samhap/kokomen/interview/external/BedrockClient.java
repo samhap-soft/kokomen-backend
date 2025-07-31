ae19a716ed923133574386a7471eec3f20207786
nhlee98 <e5slnh9876@gmail.com>
2025-07-31 22:52:55 +0900

    fix: 잘못된 모델명 변경

--- src/main/java/com/samhap/kokomen/interview/external/BedrockClient.java
+++ src/main/java/com/samhap/kokomen/interview/external/BedrockClient.java
@@ -21,10 +21,10 @@ import software.amazon.awssdk.services.bedrockruntime.model.Message;
 public class BedrockClient {
 
     private static final List<String> MODEL_IDS = List.of(
-            "anthropic.claude-3-sonnet-20240229-v1:0",
-            "anthropic.claude-3-5-sonnet-20240620-v1:0",
-            "anthropic.claude-3-5-sonnet-20241022-v2:0",
-            "anthropic.claude-3-7-sonnet-20250219-v1:0",
+            "apac.anthropic.claude-3-sonnet-20240229-v1:0",
+            "apac.anthropic.claude-3-5-sonnet-20240620-v1:0",
+            "apac.anthropic.claude-3-5-sonnet-20241022-v2:0",
+            "apac.anthropic.claude-3-7-sonnet-20250219-v1:0",
             "apac.anthropic.claude-sonnet-4-20250514-v1:0"
     );
 
