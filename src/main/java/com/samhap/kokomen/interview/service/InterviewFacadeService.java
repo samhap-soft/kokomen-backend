19db24450f8f31cb2c6514afc963dfef6598fd12
nhlee98 <e5slnh9876@gmail.com>
2025-07-31 16:09:50 +0900

    chore: 디버깅을 위한 임시 로그 추가

--- src/main/java/com/samhap/kokomen/interview/service/InterviewFacadeService.java
+++ src/main/java/com/samhap/kokomen/interview/service/InterviewFacadeService.java
@@ -174,6 +174,8 @@ public class InterviewFacadeService {
     }
 
     private void callbackBedrock(ConverseResponse converseResponse, Long memberId, QuestionAndAnswers questionAndAnswers, Long interviewId, String lockKey) {
+        log.info("memberId: {}, interviewId: {}, curQuestionId: {}, Bedrock API 응답: {}", memberId, interviewId, questionAndAnswers.readCurQuestion().getId(),
+                converseResponse);
         String rawText = converseResponse.output().message().content().get(0).text();
         String cleanedContent = cleanJsonContent(rawText);
 
