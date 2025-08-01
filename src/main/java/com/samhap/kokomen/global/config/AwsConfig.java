924539d1fe04aa5c762a9f3d32bc4da873223534
nhlee98 <e5slnh9876@gmail.com>
2025-08-01 19:49:55 +0900

    feat: 비동기 블록 스레드 풀 및 블로킹 베드락 클라이언트 커넥션 수 증가

--- src/main/java/com/samhap/kokomen/global/config/AwsConfig.java
+++ src/main/java/com/samhap/kokomen/global/config/AwsConfig.java
@@ -3,6 +3,7 @@ package com.samhap.kokomen.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeAsyncClient;
@@ -15,6 +16,12 @@ public class AwsConfig {
     public BedrockRuntimeClient bedrockRuntimeClient() {
         return BedrockRuntimeClient.builder()
                 .credentialsProvider(InstanceProfileCredentialsProvider.create())
+                .httpClientBuilder(ApacheHttpClient.builder()
+                        .maxConnections(1000)
+                        .connectionAcquisitionTimeout(java.time.Duration.ofSeconds(10))
+                        .connectionTimeout(java.time.Duration.ofSeconds(3))
+                        .socketTimeout(java.time.Duration.ofSeconds(20))
+                )
                 .region(Region.AP_NORTHEAST_2)
                 .build();
     }
@@ -24,7 +31,7 @@ public class AwsConfig {
         return BedrockRuntimeAsyncClient.builder()
                 .credentialsProvider(InstanceProfileCredentialsProvider.create())
                 .httpClientBuilder(NettyNioAsyncHttpClient.builder()
-                        .maxConcurrency(1000)
+                        .maxConcurrency(100)
                         .connectionAcquisitionTimeout(java.time.Duration.ofSeconds(10))
                         .connectionTimeout(java.time.Duration.ofSeconds(3))
                         .readTimeout(java.time.Duration.ofSeconds(20))
