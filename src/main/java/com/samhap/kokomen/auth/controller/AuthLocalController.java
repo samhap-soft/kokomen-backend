ef9f39ca9ed3b613100ff52b71913323440ab3fc
devhoya97 <diajbh123@gmail.com>
2025-08-05 14:44:23 +0900

    feat: load-test에서 로그인 쉽게 하기 위한 프로파일 설정

--- src/main/java/com/samhap/kokomen/auth/controller/AuthLocalController.java
+++ src/main/java/com/samhap/kokomen/auth/controller/AuthLocalController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("local")
+@Profile("local|load-test")
 @RequiredArgsConstructor
 @RestController
 public class AuthLocalController {
