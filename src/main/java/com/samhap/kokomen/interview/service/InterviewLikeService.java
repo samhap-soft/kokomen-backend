ae85e878517d3b638189e49bb1f856389e2da5c2
devhoya97 <diajbh123@gmail.com>
2025-08-05 20:48:12 +0900

    refactor: 데드락 문제 발생하여, 정확하 원인 분석을 위해 좋아요 예외 발생코드 원복

--- src/main/java/com/samhap/kokomen/interview/service/InterviewLikeService.java
+++ src/main/java/com/samhap/kokomen/interview/service/InterviewLikeService.java
@@ -1,5 +1,6 @@
 package com.samhap.kokomen.interview.service;
 
+import com.samhap.kokomen.global.exception.BadRequestException;
 import com.samhap.kokomen.interview.domain.Interview;
 import com.samhap.kokomen.interview.domain.InterviewLike;
 import com.samhap.kokomen.interview.repository.InterviewLikeRepository;
@@ -22,8 +23,7 @@ public class InterviewLikeService {
 
     private void validateNotAlreadyLiked(Interview interview, Member member) {
         if (interviewLikeRepository.existsByMemberIdAndInterviewId(member.getId(), interview.getId())) {
-            // TODO: 성능 테스트를 위해 한 회원이 하나의 인터뷰에 중복 좋아요 가능하도록 설정
-//            throw new BadRequestException("이미 좋아요를 누른 인터뷰입니다.");
+            throw new BadRequestException("이미 좋아요를 누른 인터뷰입니다.");
         }
     }
 }
