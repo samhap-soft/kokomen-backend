42953e74446d5132253f5d489368ee0fbab55388
devhoya97 <diajbh123@gmail.com>
2025-08-07 16:26:11 +0900

    chore: load-test용 cd 기준 브랜치 제거

--- consumer/src/main/java/com/samhap/kokomen/KokomenInterviewConsumerApplication.java
+++ consumer/src/main/java/com/samhap/kokomen/KokomenInterviewConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// cd 트리거용 더미 주석
 @EnableJpaAuditing
 @SpringBootApplication
 public class KokomenInterviewConsumerApplication {
