e132235932bee2f34fbe4b27cf69182f6ee5203c
devhoya97 <diajbh123@gmail.com>
2025-08-07 16:18:29 +0900

    chore: cd 기준 브랜치 변경 및 변경사항 생성을 위해 기존 더미 주석 제거

--- consumer/src/main/java/com/samhap/kokomen/KokomenInterviewConsumerApplication.java
+++ consumer/src/main/java/com/samhap/kokomen/KokomenInterviewConsumerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// cd 테스트를 위해 변경사항 만들기 용 주석
 @EnableJpaAuditing
 @SpringBootApplication
 public class KokomenInterviewConsumerApplication {
