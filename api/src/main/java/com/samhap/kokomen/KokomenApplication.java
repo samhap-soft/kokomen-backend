a997dd024d606c5e032b4db03db9d32e4f3083a3
devhoya97 <diajbh123@gmail.com>
2025-08-07 16:04:35 +0900

    chore: ci시 테스트 결과 확인 경로 변경 및 cd를 돌리기 위한 더미 주석 추가

--- api/src/main/java/com/samhap/kokomen/KokomenApplication.java
+++ api/src/main/java/com/samhap/kokomen/KokomenApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// cd 테스트를 위해 변경사항 만들기 용 주석
 @EnableRetry
 @EnableJpaAuditing
 @EnableScheduling
--- consumer/src/main/java/com/samhap/kokomen/KokomenInterviewConsumerApplication.java
+++ consumer/src/main/java/com/samhap/kokomen/KokomenInterviewConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// cd 테스트를 위해 변경사항 만들기 용 주석
 @EnableJpaAuditing
 @SpringBootApplication
 public class KokomenInterviewConsumerApplication {
