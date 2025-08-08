044286b27f0f43620fc389e2b97c4ab7fe693ad5
와일드카고 <146502065+kargowild@users.noreply.github.com>
2025-08-08 16:26:45 +0900

    [Feat] - RootQuestion에 대한 커스텀 메트릭 AOP 기능 (#184)
    * feat: RootQuestion마다 진행도, 답변 비율 확인을 위한 커스텀 메트릭 추가
    
    * chore: dev 환경에서 테스트를 위한 cd rule 변경
    
    * feat: 회원의 점수가 0점 아래로 떨어지지 않도록 변경
    
    * refactor: 꼬리질문에 대한 답변 랭크 커스텀 매트릭 제거. 커스텀 매트릭 찍는 로직에서 최대한 가벼운 DB 쿼리를 사용하도록 최적화
    
    * refactor: 가독성 고려하여 메서드명 변경, 개행 추가
    
    * chore: 임시로 변경한 cd-dev 원복
    
    * refactor: EOL 추가
    
    * refactor: 사용하지 않는 메서드 제거
    
    * fix: 메트릭을 찍는 커스텀 AOP의 Order를 1로 설정해 트랜잭션 커밋 이후 수행되도록 변경
    
    * refactor: 이미 Order=1 AOP가 존재하므로, RootQuestionMetricAspect의 Order를 3으로 변경
    
    * feat: async 요청에 대해서도 커스텀메트릭이 증가하는 기능 구현
    
    * refactor: TODO 주석 작성

--- src/main/java/com/samhap/kokomen/interview/repository/QuestionRepository.java
+++ src/main/java/com/samhap/kokomen/interview/repository/QuestionRepository.java
@@ -4,6 +4,7 @@ import com.samhap.kokomen.interview.domain.Interview;
 import com.samhap.kokomen.interview.domain.Question;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
@@ -12,6 +13,9 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findByInterviewOrderById(Interview interview);
 
     int countByInterview(Interview interview);
+
+    @Query(value = "SELECT q.id FROM question q WHERE q.interview_id = :interviewId ORDER BY q.id ASC LIMIT 1", nativeQuery = true)
+    Long findFirstQuestionIdByInterviewIdOrderByIdAsc(Long interviewId);
 
     List<Question> findTop2ByInterviewIdOrderByIdDesc(Long interviewId);
 }
