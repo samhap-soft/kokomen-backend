8b1728fcef50f46775e2b0b981943bfc9183126d
devhoya97 <diajbh123@gmail.com>
2025-08-08 16:13:38 +0900

    feat: async 요청에 대해서도 커스텀메트릭이 증가하는 기능 구현

--- src/main/java/com/samhap/kokomen/global/aop/RootQuestionMetricAspect.java
+++ src/main/java/com/samhap/kokomen/global/aop/RootQuestionMetricAspect.java
@@ -1,8 +1,14 @@
 package com.samhap.kokomen.global.aop;
 
+import com.samhap.kokomen.answer.domain.Answer;
 import com.samhap.kokomen.answer.domain.AnswerRank;
+import com.samhap.kokomen.answer.service.AnswerService;
+import com.samhap.kokomen.interview.domain.Interview;
+import com.samhap.kokomen.interview.domain.InterviewState;
+import com.samhap.kokomen.interview.domain.QuestionAndAnswers;
 import com.samhap.kokomen.interview.repository.InterviewRepository;
 import com.samhap.kokomen.interview.repository.QuestionRepository;
+import com.samhap.kokomen.interview.service.InterviewService;
 import com.samhap.kokomen.interview.service.dto.InterviewProceedResponse;
 import com.samhap.kokomen.interview.service.dto.InterviewStartResponse;
 import io.micrometer.core.instrument.MeterRegistry;
@@ -22,6 +28,8 @@ import org.springframework.stereotype.Component;
 public class RootQuestionMetricAspect {
 
     private final MeterRegistry meterRegistry;
+    private final InterviewService interviewService;
+    private final AnswerService answerService;
     private final InterviewRepository interviewRepository;
     private final QuestionRepository questionRepository;
 
@@ -62,6 +70,38 @@ public class RootQuestionMetricAspect {
         boolean isRootQuestionAnswer = Objects.equals(curQuestionId, firstQuestionId);
         if (isRootQuestionAnswer) {
             AnswerRank answerRank = result.get().curAnswerRank();
+            Long rootQuestionId = interviewRepository.findRootQuestionIdByInterviewId(interviewId);
+            meterRegistry.counter(
+                    "root_question_answer_rank_count_" + answerRank.name().toLowerCase(),
+                    "root_question_id", String.valueOf(rootQuestionId)
+            ).increment();
+        }
+    }
+
+    @Pointcut("execution(* com.samhap.kokomen.interview.service.InterviewProceedBlockAsyncService.proceedOrEndInterviewBlockAsync(..)) && args(memberId, questionAndAnswers, interviewId)")
+    public void asyncProceedInterviewPointcut(Long memberId, QuestionAndAnswers questionAndAnswers, Long interviewId) {
+    }
+
+    @AfterReturning(pointcut = "asyncProceedInterviewPointcut(memberId, questionAndAnswers, interviewId)")
+    public void increaseRootQuestionInterviewEndCountByAsyncProceed(Long memberId, QuestionAndAnswers questionAndAnswers, Long interviewId) {
+        Interview interview = interviewService.readInterview(interviewId);
+        if (interview.getInterviewState() == InterviewState.FINISHED) {
+            Long rootQuestionId = interviewRepository.findRootQuestionIdByInterviewId(interviewId);
+            meterRegistry.counter(
+                    "root_question_interview_end_count_total",
+                    "root_question_id", String.valueOf(rootQuestionId)
+            ).increment();
+        }
+    }
+
+    @AfterReturning(pointcut = "asyncProceedInterviewPointcut(memberId, questionAndAnswers, interviewId)")
+    public void increaseRootQuestionAnswerRankCountByAsyncProceed(Long memberId, QuestionAndAnswers questionAndAnswers, Long interviewId) {
+        Long curQuestionId = questionAndAnswers.readCurQuestion().getId();
+        Long firstQuestionId = questionRepository.findFirstQuestionIdByInterviewIdOrderByIdAsc(interviewId);
+        boolean isRootQuestionAnswer = Objects.equals(curQuestionId, firstQuestionId);
+        if (isRootQuestionAnswer) {
+            Answer answer = answerService.readByQuestionId(curQuestionId);
+            AnswerRank answerRank = answer.getAnswerRank();
             Long rootQuestionId = interviewRepository.findRootQuestionIdByInterviewId(interviewId);
             meterRegistry.counter(
                     "root_question_answer_rank_count_" + answerRank.name().toLowerCase(),
