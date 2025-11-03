5b7fa93d05630f9052c2d8e94f4c829b8ddb3d1b
unifolio0 <boho82ha@gmail.com>
2025-11-03 18:56:21 +0900

    chore: cd 트리거 수정

--- common/src/main/java/com/samhap/kokomen/member/repository/MemberRepository.java
+++ common/src/main/java/com/samhap/kokomen/member/repository/MemberRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    
+
     @Query(value = """
             SELECT COUNT(*) + 1
             FROM member
