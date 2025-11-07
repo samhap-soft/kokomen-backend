baf7d2421cae8cca428071720eeff754a63db341
unifolio0 <boho82ha@gmail.com>
2025-11-07 22:03:57 +0900

    feat: 임시 S3 url 수정

--- api/src/main/java/com/samhap/kokomen/recruit/schedular/service/ImageDownloadService.java
+++ api/src/main/java/com/samhap/kokomen/recruit/schedular/service/ImageDownloadService.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestClient;
 public class ImageDownloadService {
 
     private static final String BASE_URL = "https://d2juy7qzamcf56.cloudfront.net/";
-    private static final String S3_BASE_PATH = "recruit/company/";
+    private static final String S3_BASE_PATH = "recruit/tmp/";
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private static final String FOLDER_DELIMITER = "/";
 
