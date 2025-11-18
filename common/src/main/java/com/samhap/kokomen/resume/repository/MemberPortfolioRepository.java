83f43db772ea57ec662001439baafdeae175db01
unifolio0 <boho82ha@gmail.com>
2025-11-18 22:14:25 +0900

    test: restdocs 테스트 작성

--- common/src/main/java/com/samhap/kokomen/resume/repository/MemberPortfolioRepository.java
+++ common/src/main/java/com/samhap/kokomen/resume/repository/MemberPortfolioRepository.java
@@ -1,7 +1,10 @@
 package com.samhap.kokomen.resume.repository;
 
 import com.samhap.kokomen.resume.domain.MemberPortfolio;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberPortfolioRepository extends JpaRepository<MemberPortfolio, Long> {
+
+    Optional<MemberPortfolio> findByMemberId(Long memberId);
 }
