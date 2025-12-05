5581e1785d6dc9c80cbdc08f128a8ca1ad5ee5f6
unifolio0 <boho82ha@gmail.com>
2025-12-05 20:35:49 +0900

    test: 이력서 평가 비동기 API test

--- api/src/main/java/com/samhap/kokomen/resume/controller/CareerMaterialsController.java
+++ api/src/main/java/com/samhap/kokomen/resume/controller/CareerMaterialsController.java
@@ -14,6 +14,7 @@ import com.samhap.kokomen.resume.service.dto.ResumeSaveRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,7 +70,7 @@ public class CareerMaterialsController {
     @GetMapping("/evaluations")
     public ResponseEntity<ResumeEvaluationHistoryResponses> findResumeEvaluationHistory(
             @Authentication MemberAuth memberAuth,
-            Pageable pageable
+            @PageableDefault(size = 20) Pageable pageable
     ) {
         return ResponseEntity.ok(careerMaterialsFacadeService.findResumeEvaluationHistory(memberAuth, pageable));
     }
