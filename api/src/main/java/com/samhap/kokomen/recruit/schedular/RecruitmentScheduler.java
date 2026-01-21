918335cccc12e1124e036ebf5a6b00f044edb870
unifolio0 <boho82ha@gmail.com>
2026-01-21 15:15:47 +0900

    chore: 공고 크롤링 임시 중단

--- api/src/main/java/com/samhap/kokomen/recruit/schedular/RecruitmentScheduler.java
+++ api/src/main/java/com/samhap/kokomen/recruit/schedular/RecruitmentScheduler.java
@@ -3,7 +3,6 @@ package com.samhap.kokomen.recruit.schedular;
 import com.samhap.kokomen.recruit.schedular.service.RecruitmentDataService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,7 +12,7 @@ public class RecruitmentScheduler {
 
     private final RecruitmentDataService recruitmentDataService;
 
-    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
+    //    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
     public void collectRecruitmentData() {
         try {
             log.info("채용 공고 수집 작업 시작");
