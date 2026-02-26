0a12afe2bf8d05ab632f27fbb14446e958a1cfac
unifolio0 <boho82ha@gmail.com>
2026-02-26 18:37:44 +0900

    refactor: 웹훅 api 헤더 타입 수정

--- src/main/java/com/samhap/kokomen/payment/controller/WebhookController.java
+++ src/main/java/com/samhap/kokomen/payment/controller/WebhookController.java
@@ -2,7 +2,6 @@ package com.samhap.kokomen.payment.controller;
 
 import com.samhap.kokomen.payment.service.WebhookService;
 import com.samhap.kokomen.payment.service.dto.WebhookPayload;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +21,7 @@ public class WebhookController {
 
     @PostMapping("/tosspayments")
     public ResponseEntity<Void> handleTosspaymentsWebhook(
-            @RequestHeader("tosspayments-webhook-transmission-time") LocalDateTime transmissionTime,
+            @RequestHeader("tosspayments-webhook-transmission-time") String transmissionTime,
             @RequestHeader("tosspayments-webhook-transmission-id") String transmissionId,
             @RequestHeader("tosspayments-webhook-transmission-retried-count") int retriedCount,
             @RequestBody WebhookPayload payload
